d1cc6c1482f4542cf5b1161937473a50f070f963
Emin <44813713+3m1n470r@users.noreply.github.com>
2019-10-21 17:58:53 +0300

    Add files via upload

---
+++ cheatsheetcreator.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+read -p "Insert file name in .txt format: "  fname
+touch $fname
+
+anscmnd="yes"   #command answer (if you want to record more than 1 command in file)
+
+while [ $anscmnd == "yes" ]
+do
+        ansconf="yes"   #config answer (if you want to record more info about 1 command)
+
+	read -p "What command are you interested in? " command
+
+        cmndpath="which"
+        cmndmanual="man"
+        cmndhelp="help"
+
+        echo "-------------------------------------------------------------------------------" >> $fname
+        echo " $command" >> $fname
+        echo "-------------------------------------------------------------------------------" >> $fname
+
+        while [ $ansconf == "yes" ]
+        do
+                read -p "What are you searching for?(сmndpath/cmndmanual/cmndhelp): " request
+                for i in "cmndpath" "cmndmanual" "cmndhelp"
+                do
+			if [ $i == $request ]
+                        then
+                                echo "          $request" >> $fname
+                                echo "-------------------------" >> $fname
+				${!request} ${command} >> $fname 2> /dev/null
+                                echo "-------------------------" >> $fname
+                        fi
+                done
+                read -p "Do you want to record more information about this command?(yes/no): " ansconf
+        done
+        read -p "Do you want to consider other commands?(yes/no): " anscmnd
+done
+       
